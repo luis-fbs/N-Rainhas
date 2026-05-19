72228d77d0a5758b2b7d5628602d11bd851aa17a
luis-fbs <luis-fbs@outlook.com>
2026-05-19 10:16:27 -0300

    Aumenta número de experimentos para 20 e formato de exportação para csv.

--- experimento.py
+++ experimento.py
@@ -1,23 +1,22 @@
 import time
 from statistics import mean
 
-import main
 from main import simulated_annealing, custo
 
 N = [8, 16, 32, 128]
 T = [10, 100, 1000]
-alpha = [0.99, 0.5]
+alpha = [0.99, 0.8, 0.5]
+
+print("N,temperatura,alpha,tempo_medio,media_iteracoes,media_conflitos,solucoes_otimas")
 
 for n in N:
-    print(f"============{n}===========")
     for t in T:
         for a in alpha:
-            print(f"N = {n}, Temperatura = {t} e alpha = {a}")
             tempos = []
             iteracoes = []
             conflitos = []
             solucoes_otimas = 0
-            for _ in range(3):
+            for _ in range(20):
                 inicio = time.time()
                 resultado, i = simulated_annealing(n, t, a)
                 fim = time.time()
@@ -28,9 +27,10 @@ for n in N:
                     solucoes_otimas += 1
                 conflitos.append(custo(resultado))
 
-            print(f"""
-                Tempo médio: {mean(tempos):.4f}
-                Média de iteracoes: {mean(iteracoes):.4f}
-                Media de conflitos: {mean(conflitos):.4f}
-                Solucoes otimas: {solucoes_otimas}
-            """)
+            print(
+                f"{n},{t},{a},"
+                f"{mean(tempos):.4f},"
+                f"{mean(iteracoes):.4f},"
+                f"{mean(conflitos):.4f},"
+                f"{solucoes_otimas}"
+            )
